f54fd3daa3fc28325f225bd1ede283fd7c1b269f
Volkov <seishikage@gmail.com>
2021-06-08 10:14:56 +0300

    Эксперементы с градиентом

--- UI/Task/Task.py
+++ UI/Task/Task.py
@@ -14,6 +14,7 @@ from PyQt5.QtGui import *
 import modules
 from ico import recource
 
+
 class Task(QWidget, modules.ORM):
     """
     Виджет задачи
@@ -29,13 +30,25 @@ class Task(QWidget, modules.ORM):
         self.name_task = name_task
         self.db = self.databases.query(modules.Task).filter_by(id=self.id_task).one()
 
+        # self.setStyleSheet('background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(255, 255, 255, 255), stop:1 rgba(0, 0, 0, 255);')
+        self.setStyleSheet('background-color: qlineargradient(spread:pad, x1: 0, y1: 0, x2: 1, y2: 1,stop: 0 white, stop: 0.4 gray,stop: 1 green); background-repeat: no-repeat;')
+        # self.setStyleSheet('background-color: qlineargradient(spread:pad, x1: 0, y1: 0, x2: 1, y2: 1,stop: 0 white, stop: 0.4 gray,stop: 1 green) no-repeat;')
+
+        """
+        background-attachment:fixed; background-repeat: no-repeat;
+        """
+
+        # p = self.palette()
+        # p.setColor(self.backgroundRole(), Qt.red)
+        # grad = QLinearGradient(QPointF(0,0), QPointF(100, 100))
+        # grad.setColorAt(0, Qt.red)
+        # grad.setColorAt(1, Qt.black)
+        # p.setColor(self.backgroundRole(), grad)
+        # p.setColor(grad)
+        # self.setPalette(p)
+
         self.setAutoFillBackground(True)
-
-        p = self.palette()
-        p.setColor(self.backgroundRole(), Qt.red)
-        self.setPalette(p)
-
-        self.setFixedHeight(50)
+        self.setFixedHeight(50)  # Фиксированный размер виджета
 
         layout = QHBoxLayout()
         layout.setContentsMargins(10, 0, 0, 0)
@@ -46,7 +59,6 @@ class Task(QWidget, modules.ORM):
         btn_close_task = ButtonComplete()
         layout.addWidget(btn_close_task)
         btn_close_task.clicked.connect(self.action_close_task)
-        # btn_close_task.setIcon(QIcon(":/check.png"))
 
         self.setLayout(layout)
 
@@ -69,6 +81,7 @@ class NameTask(QLabel):
 
     def __init__(self, text):
         super().__init__(text)
+        # self.setStyleSheet('background-attachment:fixed; background-repeat: no-repeat;')
 
         font = QFont()
         font.setPointSize(24)
@@ -81,7 +94,10 @@ class ButtonComplete(QPushButton):
     def __init__(self):
         super().__init__()
 
+        # self.setAutoFillBackground(True)
+        # self.setStyleSheet('background-attachment:fixed; background-repeat: no-repeat;')
+
         self.setIcon(QIcon(":/check.png"))
         self.setIconSize(QSize(50, 50))
         self.setFixedSize(QSize(50, 50))
-        self.setFlat(True)
+        # self.setFlat(True)

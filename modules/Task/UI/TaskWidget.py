ab8f60a9971723d42ff203f5b134dc1e3d2d30ac
Volkov <seishikage@gmail.com>
2021-06-18 17:25:19 +0300

    2.2.14 - Fix при создании новой БД.

--- modules/Task/UI/TaskWidget.py
+++ modules/Task/UI/TaskWidget.py
@@ -40,7 +40,7 @@ class TaskWidget(QWidget, ORM.ORM):
                 if not task.completed:
                     self.layout.addWidget(modules.Task.UI.Task(id_task=task.id, name_task=task.task_name))
         except sqlalchemy.exc.OperationalError:
-            pass
+            print("sqlalchemy.exc.OperationalError")
 
     def create_task(self, name_task):
         """
@@ -51,18 +51,23 @@ class TaskWidget(QWidget, ORM.ORM):
         """
 
         # Добавление задачи в БД
-        self.databases.add(ORM.Task(
-            task_name=name_task,
-            date_created=datetime.datetime.now().date()
-        ))
-        self.databases.commit()
+        try:
+            self.databases.add(ORM.Task(
+                task_name=name_task,
+                date_created=datetime.datetime.now().date()
+            ))
+            self.databases.commit()
+        except sqlalchemy.exc.OperationalError:
+            print("sqlalchemy.exc.OperationalError")
+        except sqlalchemy.exc.PendingRollbackError:
+            print("sqlalchemy.exc.PendingRollbackError")
+        else:
+            # Получение последнего ИД (решения вопроса с получение ИД задачи)
+            last_id_task = self.databases.query(ORM.Task).order_by(ORM.Task.id.desc()).first()
 
-        # Получение последнего ИД (решения вопроса с получение ИД задачи)
-        last_id_task = self.databases.query(ORM.Task).order_by(ORM.Task.id.desc()).first()
-
-        # Создаем виджет задачи и добавлего для отображения
-        task = modules.Task.UI.Task(id_task=last_id_task.id, name_task=name_task)
-        self.layout.addWidget(task)
+            # Создаем виджет задачи и добавлего для отображения
+            task = modules.Task.UI.Task(id_task=last_id_task.id, name_task=name_task)
+            self.layout.addWidget(task)
 
     @classmethod
     def get_action_task(cls) -> list:
